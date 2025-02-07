0869d25d7878343725c306451b09f05d812b0ddd
William <willie@elaba.net>
2025-02-07 23:33:21 +0100

    entry doesn't have a client attribute

--- custom_components/chitubox_printer/__init__.py
+++ custom_components/chitubox_printer/__init__.py
@@ -82,7 +82,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     if unload_ok:
-        entry.client.disconnect()
+        entry.runtime_data.client.disconnect()
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
